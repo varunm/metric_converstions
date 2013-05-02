2c618ec58175190f07d1c6783f135f78c5024f4e
Alex Egan <aegan@andrew.cmu.edu>
2013-05-02 12:54:40 -0400

    added method headers... again

--- lib/metric_conversions/temperatures.rb
+++ lib/metric_conversions/temperatures.rb
@@ -1,3 +1,28 @@
 module MetricConversions
   # Your code goes here...
+
+  # Kelvin to Celsius
+  def from_K_to_C
+  end
+
+  # Kelvin to Fahrenheit
+  def from_K_to_F
+  end
+
+  # Fahrenheit to Celsius
+  def from_F_to_C
+  end
+
+  # Fahrenheit to Kelvin
+  def from_F_to_K
+  end
+
+  # Celsius to Fahrenheit
+  def from_C_to_F
+  end
+
+  # Celsius to Kelvin
+  def from_C_to_K
+  end
+  
 end
